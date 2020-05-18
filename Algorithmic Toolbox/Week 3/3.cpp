65fc539127ae74dccfd566d0c32a94ae92c6169a
nikhiljsk <nikhiljsk98@gmail.com>
2020-05-19 04:13:41 +0530

    car fueling

--- Algorithmic Toolbox/Week 3/3.cpp
+++ Algorithmic Toolbox/Week 3/3.cpp
@@ -1,28 +1,34 @@
 #include <iostream>
+#include <vector>
 
-using namespace std;
+using std::cin;
+using std::cout;
+using std::vector;
+using std::max;
 
 int cal_stop_points(int d, int m, int n, int *arr){
-    int currR=0, count=0;
-    while(currR <= n){
-        int prevR = currR;
-        if(d<=m)
-            return 0;
-        while (currR<=n && (arr[currR+1]-arr[prevR]) <= m)
-            currR += 1;
-        // cout<<currR<<prevR<<"here"<<endl;
-        if(currR == n-1)
-            return count;
-        if(currR == prevR)
+    int currPos = 0, prevPos = 0, refillCount = 0;
+    while(currPos < n){
+        while((currPos < n-1) && (arr[currPos+1]-arr[prevPos])<=m)
+            currPos += 1;
+        
+        if(currPos == n-1)
+            return refillCount;
+
+        if(currPos < n)
+            refillCount += 1;
+            
+        if(currPos==prevPos)
             return -1;
-        if(currR <= n)
-            count += 1;
+        
+        prevPos = currPos;
+        
     }
-    return count;
+    return -1;
 }
 
-int main()
-{
+
+int main() {
     int d, m, n, i;
     cin>>d>>m>>n;
     int arr[n+2];
@@ -31,5 +37,6 @@ int main()
         cin>>arr[i];
     arr[n+1] = d;
     cout<<cal_stop_points(d, m, n+2, arr);
+
     return 0;
 }
